344c91fc923f178841ef2a04386bc285214dc935
geo1811 <163294787+geo1811@users.noreply.github.com>
2024-03-13 08:42:28 -0300

    Add files via upload

---
+++ aula/calculdora.py
@@ -0,0 +1,13 @@
+numero1 = float(input("digite o primeiro numero"))
+numero2 = float(input("digite o segundo numero"))
+
+soma = numero1+ numero2
+subtracao= numero1-numero2
+mutplicacao= numero1 * numero2
+divisao=numero1/numero2
+
+print('a soma é ',soma)
+print('a subtracao é',subtracao)
+print('a multiplicacao é',mutplicacao)
+print(' a ddivisao é',divisao)
+
